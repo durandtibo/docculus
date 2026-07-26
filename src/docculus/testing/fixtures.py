f2ecbb2b8d6a317bda0c25c04e17a90f19c62ecb
Thibaut Durand <durand.tibo@gmail.com>
2026-07-25 19:12:17 -0700

    Add `faker` optional dependency (#9)

--- src/docculus/testing/fixtures.py
+++ src/docculus/testing/fixtures.py
@@ -5,11 +5,18 @@ r"""Define some pytest fixtures for testing.
 
 from __future__ import annotations
 
-__all__ = ["persista_available", "persista_not_available"]
+__all__ = ["faker_available", "faker_not_available", "persista_available", "persista_not_available"]
 
 import pytest
 
-from docculus.utils.imports import is_persista_available
+from docculus.utils.imports import is_faker_available, is_persista_available
+
+faker_available: pytest.MarkDecorator = pytest.mark.skipif(
+    not is_faker_available(), reason="Requires faker"
+)
+faker_not_available: pytest.MarkDecorator = pytest.mark.skipif(
+    is_faker_available(), reason="Skip if faker is available"
+)
 
 persista_available: pytest.MarkDecorator = pytest.mark.skipif(
     not is_persista_available(), reason="Requires persista"
--- src/docculus/utils/imports/__init__.py
+++ src/docculus/utils/imports/__init__.py
@@ -3,13 +3,22 @@ r"""Helpers to detect and validate optional dependencies."""
 from __future__ import annotations
 
 __all__ = [
+    "check_faker",
     "check_persista",
+    "faker_available",
+    "is_faker_available",
     "is_persista_available",
     "persista_available",
+    "raise_faker_missing_error",
     "raise_persista_missing_error",
 ]
 
-
+from docculus.utils.imports.faker import (
+    check_faker,
+    faker_available,
+    is_faker_available,
+    raise_faker_missing_error,
+)
 from docculus.utils.imports.persista import (
     check_persista,
     is_persista_available,
