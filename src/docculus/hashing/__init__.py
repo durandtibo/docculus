8f79a2cf989c94be5f5c761f6d0b347a75d13dcb
Thibaut Durand <durand.tibo@gmail.com>
2026-07-25 19:54:46 -0700

    Add `hash_document` (#14)

--- src/docculus/hashing/__init__.py
+++ src/docculus/hashing/__init__.py
@@ -2,6 +2,7 @@ r"""Contain hashing utilities."""
 
 from __future__ import annotations
 
-__all__ = ["hash_document_uuid"]
+__all__ = ["hash_document", "hash_document_uuid", "hash_documents"]
 
+from docculus.hashing.hashing import hash_document, hash_documents
 from docculus.hashing.uuid import hash_document_uuid
