4f0f3069f6c4c0302855e5b6804913c14e606122
Thibaut Durand <durand.tibo@gmail.com>
2026-07-25 19:24:39 -0700

    Add code to analyze documents

---
+++ src/docculus/analysis/__init__.py
@@ -0,0 +1,37 @@
+r"""Contain utilities to compute document statistics."""
+
+from __future__ import annotations
+
+__all__ = [
+    "ApproxContentStats",
+    "ExactContentStats",
+    "MetadataStats",
+    "compute_content_stats_approx",
+    "compute_content_stats_exact",
+    "compute_metadata_stats",
+    "find_duplicate_content_document_ids",
+    "find_empty_document_ids",
+    "find_empty_documents",
+    "print_content_stats_report",
+    "print_metadata_stats_report",
+]
+
+from docculus.documents.analysis.content_approx import (
+    ApproxContentStats,
+    compute_content_stats_approx,
+)
+from docculus.documents.analysis.content_exact import (
+    ExactContentStats,
+    compute_content_stats_exact,
+)
+from docculus.documents.analysis.content_print import print_content_stats_report
+from docculus.documents.analysis.duplicate import find_duplicate_content_document_ids
+from docculus.documents.analysis.empty import (
+    find_empty_document_ids,
+    find_empty_documents,
+)
+from docculus.documents.analysis.metadata_print import print_metadata_stats_report
+from docculus.documents.analysis.metadata_stats import (
+    MetadataStats,
+    compute_metadata_stats,
+)
