40740ad26bf7f908988e1d16c6419b938f3b247d
Thibaut Durand <durand.tibo@gmail.com>
2026-07-25 19:59:03 -0700

    Add `DocumentHasher`

--- src/docculus/hashing/__init__.py
+++ src/docculus/hashing/__init__.py
@@ -2,7 +2,14 @@ r"""Contain hashing utilities."""
 
 from __future__ import annotations
 
-__all__ = ["hash_document", "hash_document_uuid", "hash_documents"]
+__all__ = [
+    "DocumentHasher",
+    "hash_document",
+    "hash_document_uuid",
+    "hash_documents",
+    "register_document_hasher",
+]
 
+from docculus.hashing.hasher import DocumentHasher, register_document_hasher
 from docculus.hashing.hashing import hash_document, hash_documents
 from docculus.hashing.uuid import hash_document_uuid
