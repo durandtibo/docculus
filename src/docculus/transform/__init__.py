57c1af8614c48d57bd9114d1a4795a5cb6211b5c
Thibaut Durand <durand.tibo@gmail.com>
2026-07-25 20:24:59 -0700

    Add `assign_ids`

--- src/docculus/transform/__init__.py
+++ src/docculus/transform/__init__.py
@@ -3,6 +3,8 @@ r"""Contain utilities to transform documents."""
 from __future__ import annotations
 
 __all__ = [
+    "assign_ids",
+    "copy_ids_to_metadata",
     "deduplicate_documents",
     "filter_by_metadata",
     "filter_by_metadata_range",
@@ -16,4 +18,5 @@ from docculus.transform.filter import (
     filter_by_metadata_range,
     filter_by_metadata_values,
 )
+from docculus.transform.id import assign_ids, copy_ids_to_metadata
 from docculus.transform.sort import sort_by_metadata
