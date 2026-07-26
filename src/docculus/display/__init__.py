1dc6e5421f5bd21049fca50c13d99bd3aa6df202
Thibaut Durand <durand.tibo@gmail.com>
2026-07-25 18:28:12 -0700

    Add `print_document`

---
+++ src/docculus/display/__init__.py
@@ -0,0 +1,8 @@
+r"""Contain utilities to print LangChain documents to the terminal."""
+
+from __future__ import annotations
+
+__all__ = ["print_document", "print_documents_metadata"]
+
+from docculus.display.document import print_document
+from docculus.display.metadata import print_documents_metadata
